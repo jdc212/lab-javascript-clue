2b0f7467bf6b198684572b6ee26b46ed975648af
Joe D <joedeejr@gmail.com>
2022-01-13 06:40:36 -0300

    SEtup2

--- tests/clue.spec.js
+++ tests/clue.spec.js
@@ -1,3 +1,5 @@
+// Suspects
+
 const suspectsArray = [
     {
       firstName: "Jacob",
@@ -61,8 +63,95 @@ const suspectsArray = [
     }
   ];
   
-  roomsArray,
-  weaponsArray,
+  // Weapons
+
+  const roomsArray = [
+    {
+      name: "rope",
+      weight: 10
+    },
+    {
+      name: "knife",
+      weight: 8
+    },
+    {
+      name: "candlestick",
+      weight: 2
+    },
+    {
+      name: "dumbbell",
+      weight: 30
+    },
+    {
+      name: "poison",
+      weight: 2
+    },
+    {
+      name: "axe",
+      weight: 15
+    },
+    {
+      name: "bat",
+      weight: 13
+    },
+    {
+      name: "trophy",
+      weight: 25
+    },
+    {
+      name: "pistol",
+      weight: 20
+    }
+  ];
+
+  // Rooms
+
+  const weaponsArray = [
+    {
+      name: "Dining Room"
+    },
+    {
+      name: "Conservatory"
+    },
+    {name: "Kitchen"
+    },
+    {
+      name: "Study"
+    },
+    {
+      name: "Library"},
+    {
+      name: "Billiard Room"
+    },
+    {
+      name: "Lounge"
+    },
+    {
+      name: "Ballroom"
+    },
+    {
+      name: "Hall"
+    },
+    {
+      name: "Spa"
+    },
+    {
+      name: "Living Room"
+    },
+    {
+      name: "Observatory"
+    },
+    {
+      name: "Theater"
+    },
+    {
+      name: "Guest House"
+    },
+    {
+      name: "Patio"
+    }
+  ];
+  
   pickMystery,
   revealMystery,
   selectRandom
